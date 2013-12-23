7a139b9c148878ce12915a55144c066f429efe3c
Michael Dominick <michael@fingertiptech.com>
2013-12-22 21:31:05 -0500

    Adding Util for checking for empty string

---
+++ NSString+Utils.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Utils.h
+//  spindle
+//
+//  Created by Michael on 12/22/13.
+//  Copyright (c) 2013 fingertiptech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Utils)
+
+- (BOOL)isNotEmpty;
+
+@end
---
+++ NSString+Utils.m
@@ -0,0 +1,25 @@
+//
+//  NSString+Utils.m
+//  spindle
+//
+//  Created by Michael on 12/22/13.
+//  Copyright (c) 2013 fingertiptech. All rights reserved.
+//
+
+#import "NSString+Utils.h"
+
+@implementation NSString (Utils)
+
+- (BOOL)isNotEmpty
+{
+    BOOL isValid = NO;
+    if (![self isKindOfClass:[NSNull class]]
+        && ![self isEqualToString:@""])
+    {
+        isValid = YES;
+    }
+    
+    return isValid;
+}
+
+@end
